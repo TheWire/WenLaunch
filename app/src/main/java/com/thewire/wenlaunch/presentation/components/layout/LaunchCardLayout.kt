5e7350b20076c7ea6f25db7e5e8fe2256839086b
TheWire <TheWire@users.noreply.github.com>
2022-02-22 23:17:44 +0000

    orientation lazy list + LaunchCardLayout

--- app/src/main/java/com/thewire/wenlaunch/presentation/components/layout/LaunchCardLayout.kt
+++ app/src/main/java/com/thewire/wenlaunch/presentation/components/layout/LaunchCardLayout.kt
@@ -1,2 +1,98 @@
 package com.thewire.wenlaunch.presentation.components.layout
 
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.Layout
+
+@Composable
+fun LaunchCardLayout(
+    modifier: Modifier = Modifier,
+    image: Painter,
+    imageDescription: String,
+    imageScale: ContentScale = ContentScale.Crop,
+    imageModifier: Modifier,
+    header: @Composable () -> Unit,
+    content: @Composable () -> Unit
+) {
+    Layout(
+        modifier = modifier,
+        content = {
+            header()
+            content()
+            Image(
+                modifier = imageModifier
+                    .animateContentSize()
+                    .fillMaxSize(),
+                painter = image,
+                contentDescription = imageDescription,
+                contentScale = imageScale,
+            )
+        }
+    ) { measurables, constraints ->
+
+        val topConstraints = constraints.copy(
+            minHeight = measurables[0].minIntrinsicHeight(constraints.maxWidth),
+            maxHeight = minOf(
+                measurables[0].maxIntrinsicHeight(constraints.minWidth),
+                constraints.maxHeight
+            )
+        )
+
+        val lowerConstraints = constraints.copy(
+            minHeight = maxOf(constraints.minHeight - topConstraints.maxHeight, 0),
+            maxHeight = constraints.maxHeight - topConstraints.minHeight,
+        )
+
+        val aspectRatio = image.intrinsicSize.height / image.intrinsicSize.width
+        val imageConstraints = lowerConstraints.copy(
+            minWidth = minOf(
+                (lowerConstraints.minHeight / aspectRatio).toInt(),
+                maxOf(
+                    constraints.minWidth - measurables[1].minIntrinsicWidth(lowerConstraints.minHeight),
+                    0
+                )
+            ),
+            maxWidth = minOf(
+                (lowerConstraints.maxHeight / aspectRatio).toInt(),
+                constraints.maxWidth - measurables[1].minIntrinsicWidth(lowerConstraints.maxHeight)
+            ),
+        )
+        val imagePlaceable = measurables[2].measure(imageConstraints)
+        val contentConstraints = lowerConstraints.copy(
+            minWidth = measurables[1].minIntrinsicWidth(lowerConstraints.minHeight),
+            maxWidth = constraints.maxWidth - imagePlaceable.width,
+        )
+
+        val contentPlaceable = measurables[1].measure(contentConstraints)
+
+
+        val headerConstraints = topConstraints.copy(
+            maxWidth = maxOf(
+                minOf(
+                    contentPlaceable.width + imagePlaceable.width,
+                    constraints.maxWidth
+                ), constraints.minWidth
+            )
+        )
+
+        val headerPlaceable = measurables[0].measure(headerConstraints)
+
+        layout(
+            headerPlaceable.width,
+            headerPlaceable.height + imagePlaceable.height
+        ) {
+            var yPosition = 0
+            headerPlaceable.placeRelative(0, 0)
+            yPosition += headerPlaceable.height
+            contentPlaceable.placeRelative(0, yPosition)
+            val imageYOffset =
+                maxOf(headerPlaceable.width - (contentPlaceable.width + imagePlaceable.width), 0)
+            imagePlaceable.placeRelative(contentPlaceable.width + imageYOffset, yPosition)
+        }
+    }
+}
--- app/src/main/java/com/thewire/wenlaunch/presentation/components/layout/LazyListOrientationLayout.kt
+++ app/src/main/java/com/thewire/wenlaunch/presentation/components/layout/LazyListOrientationLayout.kt
@@ -1,2 +1,54 @@
 package com.thewire.wenlaunch.presentation.components.layout
 
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.unit.Dp
+import kotlinx.coroutines.flow.collect
+
+@Composable
+fun LazListOrientationLayout(
+    modifier: Modifier = Modifier,
+    contentModifier: Modifier = Modifier,
+    portraitMaxHeight: Dp,
+    portraitMinHeight: Dp = portraitMaxHeight,
+    landscapeMaxWidth: Dp,
+    landscapeMinWidth: Dp = landscapeMaxWidth,
+    content: LazyListScope.(Modifier) -> Unit = {}
+) {
+    val configuration = LocalConfiguration.current
+    val orientation = remember { mutableStateOf(Configuration.ORIENTATION_PORTRAIT) }
+    LaunchedEffect(configuration) {
+        snapshotFlow { configuration.orientation }
+            .collect { orientation.value = it }
+    }
+
+    when (orientation.value) {
+        Configuration.ORIENTATION_LANDSCAPE -> {
+            LazyRow(
+                modifier = modifier,
+            ) {
+                content(
+                    contentModifier.fillMaxHeight().widthIn(landscapeMinWidth, landscapeMaxWidth)
+                )
+            }
+        }
+        Configuration.ORIENTATION_PORTRAIT -> {
+            LazyColumn(
+                modifier = modifier,
+            ) {
+                content(
+                    contentModifier.fillMaxWidth().heightIn(portraitMinHeight, portraitMaxHeight)
+                )
+            }
+        }
+    }
+}
